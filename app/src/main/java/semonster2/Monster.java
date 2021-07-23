193ac98a2c90ddae1f4342026b725078af10b128
ringsuzune4946 <tkyu.ft@gmail.com>
2021-07-23 23:11:09 +0900

    モンスターが進化できるように．monster.javaのテストコード追加

--- app/src/main/java/semonster2/Monster.java
+++ app/src/main/java/semonster2/Monster.java
@@ -1,20 +1,22 @@
 package semonster2;
 
-import java.util.Random;
-
 public class Monster {
   String name;
   int rare;// 1:normal,2:uncommon,3:rare,4:ultra rare
 
-  Monster() {
-    Random random = new Random();
-    this.name = this.summonMonster(random.nextInt(5));
-    this.rare = random.nextInt(5);
+  Monster(int nameNum, int rareNum) {
+    this.rare = rareNum;
+    this.name = this.summonMonster(nameNum, rareNum);
   }
 
-  String summonMonster(int mnumber) {
+  String summonMonster(int mnumber, int rnumber) {
     String monsters[] = { "スライム", "サハギン", "ドラゴン", "デュラハン", "シーサーペント" };
-    return monsters[mnumber];
+    String evolve[] = { "キングスライム", "伝説のフィッシャーマン", "アクノロギア", "ベルディア", "リヴァイアサン" };
+
+    if (rnumber >= 3)
+      return evolve[mnumber];
+    else
+      return monsters[mnumber];
   }
 
   @Override
--- app/src/test/java/semonster2/MonsterTest.java
+++ app/src/test/java/semonster2/MonsterTest.java
@@ -4,5 +4,73 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MonsterTest {
+  @Test
+  public void testSummonMonster0() {
+    Monster monster = new Monster(0, 0);
+    assertEquals("スライム:レア度[0]", monster.toString());
+    Monster monster2 = new Monster(0, 1);
+    assertEquals("スライム:レア度[1]", monster2.toString());
+    Monster monster3 = new Monster(0, 2);
+    assertEquals("スライム:レア度[2]", monster3.toString());
+    Monster monster4 = new Monster(0, 3);
+    assertEquals("キングスライム:レア度[3]", monster4.toString());
+    Monster monster5 = new Monster(0, 4);
+    assertEquals("キングスライム:レア度[4]", monster5.toString());
+  }
 
+  @Test
+  public void testSummonMonster1() {
+    Monster monster = new Monster(1, 0);
+    assertEquals("サハギン:レア度[0]", monster.toString());
+    Monster monster2 = new Monster(1, 1);
+    assertEquals("サハギン:レア度[1]", monster2.toString());
+    Monster monster3 = new Monster(1, 2);
+    assertEquals("サハギン:レア度[2]", monster3.toString());
+    Monster monster4 = new Monster(1, 3);
+    assertEquals("伝説のフィッシャーマン:レア度[3]", monster4.toString());
+    Monster monster5 = new Monster(1, 4);
+    assertEquals("伝説のフィッシャーマン:レア度[4]", monster5.toString());
+  }
+
+  @Test
+  public void testSummonMonster2() {
+    Monster monster = new Monster(2, 0);
+    assertEquals("ドラゴン:レア度[0]", monster.toString());
+    Monster monster2 = new Monster(2, 1);
+    assertEquals("ドラゴン:レア度[1]", monster2.toString());
+    Monster monster3 = new Monster(2, 2);
+    assertEquals("ドラゴン:レア度[2]", monster3.toString());
+    Monster monster4 = new Monster(2, 3);
+    assertEquals("アクノロギア:レア度[3]", monster4.toString());
+    Monster monster5 = new Monster(2, 4);
+    assertEquals("アクノロギア:レア度[4]", monster5.toString());
+  }
+
+  @Test
+  public void testSummonMonster3() {
+    Monster monster = new Monster(3, 0);
+    assertEquals("デュラハン:レア度[0]", monster.toString());
+    Monster monster2 = new Monster(3, 1);
+    assertEquals("デュラハン:レア度[1]", monster2.toString());
+    Monster monster3 = new Monster(3, 2);
+    assertEquals("デュラハン:レア度[2]", monster3.toString());
+    Monster monster4 = new Monster(3, 3);
+    assertEquals("ベルディア:レア度[3]", monster4.toString());
+    Monster monster5 = new Monster(3, 4);
+    assertEquals("ベルディア:レア度[4]", monster5.toString());
+  }
+
+  @Test
+  public void testSummonMonster4() {
+    Monster monster = new Monster(4, 0);
+    assertEquals("シーサーペント:レア度[0]", monster.toString());
+    Monster monster2 = new Monster(4, 1);
+    assertEquals("シーサーペント:レア度[1]", monster2.toString());
+    Monster monster3 = new Monster(4, 2);
+    assertEquals("シーサーペント:レア度[2]", monster3.toString());
+    Monster monster4 = new Monster(4, 3);
+    assertEquals("リヴァイアサン:レア度[3]", monster4.toString());
+    Monster monster5 = new Monster(4, 4);
+    assertEquals("リヴァイアサン:レア度[4]", monster5.toString());
+  }
 }
