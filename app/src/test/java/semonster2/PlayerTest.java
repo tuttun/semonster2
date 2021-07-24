ef73d835c29418c8b1ca7a30e328c233e9fabc77
tuttun <higsumi@gmail.com>
2021-07-24 12:53:50 +0900

    PlayerTest.javaにPlayer.javaのテスト実装

--- app/src/test/java/semonster2/PlayerTest.java
+++ app/src/test/java/semonster2/PlayerTest.java
@@ -3,74 +3,69 @@ package semonster2;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+
 public class PlayerTest {
   @Test
-  public void testSummonMonster0() {
-    Monster monster = new Monster(0, 0);
-    assertEquals("スライム:レア度[0]", monster.toString());
-    Monster monster2 = new Monster(0, 1);
-    assertEquals("スライム:レア度[1]", monster2.toString());
-    Monster monster3 = new Monster(0, 2);
-    assertEquals("スライム:レア度[2]", monster3.toString());
-    Monster monster4 = new Monster(0, 3);
-    assertEquals("キングスライム:レア度[3]", monster4.toString());
-    Monster monster5 = new Monster(0, 4);
-    assertEquals("キングスライム:レア度[4]", monster5.toString());
+  public void PlayerStatus1() {
+    LinkedList<Integer> randomtestList = new LinkedList<>();// ランダムな数値を格納していく
+    String testName = "user";
+
+    randomtestList.add(0);
+    randomtestList.add(0);
+    randomtestList.add(1);
+    randomtestList.add(0);
+    randomtestList.add(0);
+    randomtestList.add(4);
+    randomtestList.add(4);
+    randomtestList.add(0);
+    randomtestList.add(3);
+    randomtestList.add(3);
+    randomtestList.add(2);
+    randomtestList.add(0);
+    randomtestList.add(1);
+    randomtestList.add(1);
+    randomtestList.add(2);
+    randomtestList.add(4);
+
+    Player player = new Player(testName, randomtestList);
+
+    String testStatus = "Deck:" + testName + "\n" + "スライム:レア度[0]\n" + "サハギン:レア度[0]\n" + "キングスライム:レア度[4]\n"
+        + "シーサーペント:レア度[0]\n" + "ベルディア:レア度[3]\n" + "ドラゴン:レア度[0]\n" + "サハギン:レア度[1]\n" + "アクノロギア:レア度[4]\n";
+
+    assertEquals(testStatus, player.toString());
+
   }
 
   @Test
-  public void testSummonMonster1() {
-    Monster monster = new Monster(1, 0);
-    assertEquals("サハギン:レア度[0]", monster.toString());
-    Monster monster2 = new Monster(1, 1);
-    assertEquals("サハギン:レア度[1]", monster2.toString());
-    Monster monster3 = new Monster(1, 2);
-    assertEquals("サハギン:レア度[2]", monster3.toString());
-    Monster monster4 = new Monster(1, 3);
-    assertEquals("伝説のフィッシャーマン:レア度[3]", monster4.toString());
-    Monster monster5 = new Monster(1, 4);
-    assertEquals("伝説のフィッシャーマン:レア度[4]", monster5.toString());
+  public void PlayerStatus2() {
+    LinkedList<Integer> randomtestList = new LinkedList<>();// ランダムな数値を格納していく
+    String testName = "hoge";
+
+    randomtestList.add(0);
+    randomtestList.add(2);
+    randomtestList.add(3);
+    randomtestList.add(1);
+    randomtestList.add(4);
+    randomtestList.add(2);
+    randomtestList.add(3);
+    randomtestList.add(0);
+    randomtestList.add(1);
+    randomtestList.add(4);
+    randomtestList.add(3);
+    randomtestList.add(0);
+    randomtestList.add(4);
+    randomtestList.add(3);
+    randomtestList.add(2);
+    randomtestList.add(1);
+
+    Player player = new Player(testName, randomtestList);
+
+    String testStatus = "Deck:" + testName + "\n" + "スライム:レア度[2]\n" + "デュラハン:レア度[1]\n" + "シーサーペント:レア度[2]\n"
+        + "デュラハン:レア度[0]\n" + "伝説のフィッシャーマン:レア度[4]\n" + "デュラハン:レア度[0]\n" + "リヴァイアサン:レア度[3]\n" + "ドラゴン:レア度[1]\n";
+
+    assertEquals(testStatus, player.toString());
+
   }
 
-  @Test
-  public void testSummonMonster2() {
-    Monster monster = new Monster(2, 0);
-    assertEquals("ドラゴン:レア度[0]", monster.toString());
-    Monster monster2 = new Monster(2, 1);
-    assertEquals("ドラゴン:レア度[1]", monster2.toString());
-    Monster monster3 = new Monster(2, 2);
-    assertEquals("ドラゴン:レア度[2]", monster3.toString());
-    Monster monster4 = new Monster(2, 3);
-    assertEquals("アクノロギア:レア度[3]", monster4.toString());
-    Monster monster5 = new Monster(2, 4);
-    assertEquals("アクノロギア:レア度[4]", monster5.toString());
-  }
-
-  @Test
-  public void testSummonMonster3() {
-    Monster monster = new Monster(3, 0);
-    assertEquals("デュラハン:レア度[0]", monster.toString());
-    Monster monster2 = new Monster(3, 1);
-    assertEquals("デュラハン:レア度[1]", monster2.toString());
-    Monster monster3 = new Monster(3, 2);
-    assertEquals("デュラハン:レア度[2]", monster3.toString());
-    Monster monster4 = new Monster(3, 3);
-    assertEquals("ベルディア:レア度[3]", monster4.toString());
-    Monster monster5 = new Monster(3, 4);
-    assertEquals("ベルディア:レア度[4]", monster5.toString());
-  }
-
-  @Test
-  public void testSummonMonster4() {
-    Monster monster = new Monster(4, 0);
-    assertEquals("シーサーペント:レア度[0]", monster.toString());
-    Monster monster2 = new Monster(4, 1);
-    assertEquals("シーサーペント:レア度[1]", monster2.toString());
-    Monster monster3 = new Monster(4, 2);
-    assertEquals("シーサーペント:レア度[2]", monster3.toString());
-    Monster monster4 = new Monster(4, 3);
-    assertEquals("リヴァイアサン:レア度[3]", monster4.toString());
-    Monster monster5 = new Monster(4, 4);
-    assertEquals("リヴァイアサン:レア度[4]", monster5.toString());
-  }
 }
